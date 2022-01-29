43ff361773f05b133c1cb970e611007c07de27b7
Ivan_Morya <mory.ivan@gmail.com>
2022-01-29 15:20:24 +0300

    ;)

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task15.task1525;
+
+public class Statics {
+    public static String FILE_NAME = "C:\\Users\\moryi\\OneDrive\\Рабочий стол\\Новый текстовый документ.txt";
+}
