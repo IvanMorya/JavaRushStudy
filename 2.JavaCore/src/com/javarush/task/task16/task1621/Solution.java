9cf9e97a9fe481d8b67520ef27f2c00befc8454b
Ivan_Morya <mory.ivan@gmail.com>
2022-02-05 11:58:38 +0300

    ;)

--- 2.JavaCore/src/com/javarush/task/task16/task1621/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1621/Solution.java
@@ -7,8 +7,11 @@ Thread.currentThread - всегда возвращает текущую нить
 public class Solution {
     static int count = 5;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ThreadNamePrinter tnp = new ThreadNamePrinter();
+        ThreadNamePrinter tnp1 = new ThreadNamePrinter();
+        tnp1.start();
+        tnp1.join();
         tnp.start();
         for (int i = 0; i < count; i++) {
             tnp.printMsg();
