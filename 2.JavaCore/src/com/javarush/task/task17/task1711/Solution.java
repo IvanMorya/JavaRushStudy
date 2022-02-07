9006b6f9367b3f7314a6e315fc66a85f8dedddee
Ivan_Morya <mory.ivan@gmail.com>
2022-02-07 21:32:08 +0300

    ;)

--- 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
@@ -1,7 +1,5 @@
 package com.javarush.task.task17.task1711;
 
-import com.javarush.task.task17.task1710.Sex;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -18,42 +16,64 @@ public class Solution {
 
     static {
         allPeople.add(Person.createMale("Иванов Иван", new Date()));  //сегодня родился    id=0
-        allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
+        allPeople.add(Person.createFemale("Петров Петр", new Date()));  //сегодня родился    id=1
     }
 
-    static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
-    static SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+    private static SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+    private static SimpleDateFormat outputFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
 
-    public static void main(String[] args) throws ParseException {
-        if (args == null || args.length < 1) {
-            throw new RuntimeException();
-        }
-        switch (args[0]) {
-            case "-c":
-                int countParam = ((args.length - 1) / 3);
-                for (int i = 1; i < countParam + 1; i++) {
-                    if (getSex(args[i + 1]) == Sex.MALE) {
-                        allPeople.add(Person.createMale(args[i], simpleDateFormat.parse(args[i + 2])));
-                    } else {
-                        allPeople.add(Person.createFemale(args[i], simpleDateFormat.parse(args[i + 2])));
+    public static void main(String[] args) {
+
+        try {
+            switch (args[0]) {
+                case "-c":
+                    synchronized (allPeople) {
+                        for (int i = 1; i < args.length; i = i + 3) {
+                            String name = args[i];
+                            String sex = args[i + 1];
+                            Date date = inputFormat.parse(args[i + 2]);
+                            Person person = sex.equals("м") ? Person.createMale(name, date) : Person.createFemale(name, date);
+                            allPeople.add(person);
+                            System.out.println(allPeople.indexOf(person));
+                        }
                     }
-                    System.out.println(allPeople.size() - 1);
-                }
-                break;
-            case "-u":
-
-                break;
-            case "-d":
-
-                break;
-            case "-i":
-
-                break;
+                    break;
+                case "-u":
+                    synchronized (allPeople) {
+                        for (int i = 1; i < args.length; i = i + 4) {
+                            int id = Integer.parseInt(args[i]);
+                            String name = args[i + 1];
+                            String sex = args[i + 2];
+                            Date date = inputFormat.parse(args[i + 3]);
+                            allPeople.get(id).setName(name);
+                            allPeople.get(id).setSex("м".equals(sex) ? Sex.MALE : Sex.FEMALE);
+                            allPeople.get(id).setBirthDate(date);
+                        }
+                    }
+                    break;
+                case "-d":
+                    synchronized (allPeople) {
+                        for (int i = 1; i < args.length; i++) {
+                            allPeople.get(Integer.parseInt(args[i])).setName(null);
+                            allPeople.get(Integer.parseInt(args[i])).setSex(null);
+                            allPeople.get(Integer.parseInt(args[i])).setBirthDate(null);
+                        }
+                    }
+                    break;
+                case "-i":
+                    synchronized (allPeople) {
+                        for (int i = 1; i < args.length; i++) {
+                            int id = Integer.parseInt(args[i]);
+                            Person person = allPeople.get(id);
+                            System.out.print(person.getName() + " ");
+                            System.out.print(person.getSex().equals(Sex.MALE) ? "м " : "ж ");
+                            System.out.println(outputFormat.format(person.getBirthDate()));
+                        }
+                    }
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-    }
-
-    private static Sex getSex(String sexParam) {
-        return sexParam.equals("м") ? com.javarush.task.task17.task1710.Sex.MALE : Sex.FEMALE;
     }
 }
