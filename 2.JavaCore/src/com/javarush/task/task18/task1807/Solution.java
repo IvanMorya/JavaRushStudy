78b4efaa9d442c80a4d3aa71b7ea917665e896fd
Ivan_Morya <mory.ivan@gmail.com>
2022-02-11 21:48:48 +0300

    ;)

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task18.task1807;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Подсчет запятых
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+
+        int commaCount = 0;
+        try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
+            while (fileInputStream.available() > 0) {
+                if (fileInputStream.read() == 44) commaCount++;
+            }
+        }
+        System.out.println(commaCount);
+
+
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        String fileName = null;
+//        int countComma = 0;
+//        try {
+//            fileName = reader.readLine();
+//            reader.close();
+//            FileInputStream fileInputStream = new FileInputStream(fileName);
+//            while (fileInputStream.available() > 0) {
+//                if (fileInputStream.read() == 44) {
+//                    countComma++;
+//                }
+//            }
+//            fileInputStream.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(countComma);
+    }
+}
