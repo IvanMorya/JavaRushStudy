bfcd4a4a4e405ef75542fa3d34816032e9002ddc
moryi <mory.ivan@gmail.com>
2022-03-11 23:44:56 +0300

    AddNewSolution

--- 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
@@ -4,57 +4,55 @@ package com.javarush.task.task19.task1920;
 Самый богатый
 */
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
 public class Solution {
     public static void main(String[] args) {
+        HashMap<String, Double> list = new HashMap<>();
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+            while (reader.ready()) {
+                String current = reader.readLine();
+                String[] split = current.split(" ");
+                if (list.containsKey(split[0])) {
+                    Double d = list.get(split[0]);
+                    list.put(split[0], d + Double.parseDouble(split[1]));
+                } else {
+                    list.put(split[0], Double.parseDouble(split[1]));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-//        TreeMap<String, Double> salary = new TreeMap<>();
-//
-//        String fileName = args[0];
-//
-//        try (BufferedReader rd = new BufferedReader(new FileReader(fileName))) {
-//            String[] splitedLine;
-//            String line;
-//            Double currentValue;
-//
-//            while ((line = rd.readLine()) != null) {
-//                splitedLine = line.split(" ");
-//                String name = splitedLine[0];
-//                double value = Double.parseDouble(splitedLine[1]);
-//
-//                if (salary.containsKey(name)) {
-//                    currentValue = salary.get(name);
-//                    salary.put(name, value + currentValue);
-//                } else {
-//                    salary.put(name, value);
-//                }
-//            }
-//        } catch (IOException ignore) {
-//            /*NOP */
-//        }
-//
-//        double maxSalary = salary.firstEntry().getValue();
-//        for (double value : salary.values()) {
-//            if (value > maxSalary) {
-//                maxSalary = value;
-//            }
-//        }
-//
-//        TreeSet<String> names = new TreeSet<>();
-//        for (String name : salary.keySet()) {
-//            if (maxSalary == salary.get(name)) {
-//                names.add(name);
-//            }
-//        }
-//
-//        for (String name : names) {
-//            System.out.println(name);
-//        }
-//
-//    }
+        double max = 0.0;
+        for (Map.Entry<String, Double> pair : list.entrySet()) {
+            if (pair.getValue() > max) {
+                max = pair.getValue();
+            }
+        }
+
+        TreeSet<String> names = new TreeSet<>();
+        for (Map.Entry<String, Double> pair : list.entrySet()) {
+            if (pair.getValue() == max) {
+                names.add(pair.getKey());
+            }
+        }
+
+        for (String name : names) {
+            System.out.println(name);
+        }
 
    /*
-   Мое решение, которое не принимает валидатор. Мои тесты с разными вариациямы входных данных отрабатывают в
+   Мой первый вариант решения, которое не принимает валидатор. Мои тесты с разными вариациямы входных данных отрабатывают в
    соответствии с требованиями задания, включая тест с входными данными которые указанны в задании.
    */
 //        List<Person1> persons = new ArrayList<>();
