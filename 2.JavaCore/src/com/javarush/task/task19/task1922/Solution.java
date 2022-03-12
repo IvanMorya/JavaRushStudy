1833d024a2df2f81caaca25f04bd947b351c4aa6
moryi <mory.ivan@gmail.com>
2022-03-13 00:12:05 +0300

    AddNewSolution

--- 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -1,11 +1,9 @@
 package com.javarush.task.task19.task1922;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /* 
 Ищем нужные строки
@@ -21,6 +19,39 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+        String fileName = null;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            fileName = reader.readLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.ready()) {
+                String current = reader.readLine();
+                if (checkWords(current)) {
+                    System.out.println(current);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean checkWords(String words) {
+        String[] word = words.split(" ");
+        int countWords = 0;
+        for (String s : word) {
+            if (Solution.words.contains(s)) {
+                countWords++;
+            }
+        }
+        if (countWords == 2) {
+            return true;
+        } else return false;
     }
 }
