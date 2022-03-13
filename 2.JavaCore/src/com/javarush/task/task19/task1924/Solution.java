5efd36a3d2c350665a5f7d2baa963c5421e001b5
moryi <mory.ivan@gmail.com>
2022-03-13 17:38:20 +0300

    AddNewSolution

--- 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
@@ -1,12 +1,11 @@
 package com.javarush.task.task19.task1924;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* 
 Замена чисел
@@ -15,7 +14,43 @@ import java.util.Map;
 public class Solution {
     public static Map<Integer, String> map = new HashMap<>();
 
-    public static void main(String[] args) {
+    static {
+        map.put(0, "ноль");
+        map.put(1, "один");
+        map.put(2, "два");
+        map.put(3, "три");
+        map.put(4, "четыре");
+        map.put(5, "пять");
+        map.put(6, "шесть");
+        map.put(7, "семь");
+        map.put(8, "восемь");
+        map.put(9, "девять");
+        map.put(10, "десять");
+        map.put(11, "одиннадцать");
+        map.put(12, "двенадцать");
+    }
 
+    public static void main(String[] args) {
+        String fileName = "";
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            fileName = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.ready()) {
+                String current = reader.readLine();
+                for (Map.Entry<Integer, String> pair : map.entrySet()) {
+                    current = current.replaceAll("\\b" + pair.getKey() + "\\b", pair.getValue());
+                }
+                System.out.println(current);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
