febe88c2cfb6d12264bcd3b99eff5a1fbd721c1c
moryi <mory.ivan@gmail.com>
2022-03-19 13:56:43 +0300

    AddNewSolution

--- 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
@@ -1,9 +1,7 @@
 package com.javarush.task.task20.task2002;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /* 
 Читаем и пишем в файл: JavaRush
@@ -14,11 +12,20 @@ public class Solution {
         //you can find your_file_name.tmp in your TMP directory or adjust outputStream/inputStream according to your file's actual location
         //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
         try {
-            File yourFile = File.createTempFile("your_file_name", null);
-            OutputStream outputStream = new FileOutputStream(yourFile);
-            InputStream inputStream = new FileInputStream(yourFile);
+//            File yourFile = File.createTempFile("your_file_name", null);
+            OutputStream outputStream = new FileOutputStream("C:\\Users\\moryi\\IdeaProjects\\JavaRushTasks\\output.txt");
+            InputStream inputStream = new FileInputStream("C:\\Users\\moryi\\IdeaProjects\\JavaRushTasks\\output.txt");
 
             JavaRush javaRush = new JavaRush();
+//            User user = new User();
+//            user.setFirstName("Ivan");
+//            user.setLastName("Morya");
+//            Calendar calendar = new GregorianCalendar();
+//            calendar.set(1987, 4, 6);
+//            user.setBirthDate(calendar.getTime());
+//            user.setMale(true);
+//            user.setCountry(User.Country.RUSSIA);
+//            javaRush.users.add(user);
             //initialize users field for the javaRush object here - инициализируйте поле users для объекта javaRush тут
             javaRush.save(outputStream);
             outputStream.flush();
@@ -26,6 +33,7 @@ public class Solution {
             JavaRush loadedObject = new JavaRush();
             loadedObject.load(inputStream);
             //here check that the javaRush object is equal to the loadedObject object - проверьте тут, что javaRush и loadedObject равны
+            System.out.println(javaRush.equals(loadedObject));
 
             outputStream.close();
             inputStream.close();
@@ -44,10 +52,44 @@ public class Solution {
 
         public void save(OutputStream outputStream) throws Exception {
             //implement this method - реализуйте этот метод
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            if (this.users != null) {
+                for (User user : this.users) {
+                    printWriter.println(user.getFirstName());
+                    printWriter.println(user.getLastName());
+                    printWriter.println(user.getBirthDate().getTime());
+                    printWriter.println(user.isMale());
+                    printWriter.println(user.getCountry().getDisplayName());
+                }
+            }
+            printWriter.close();
         }
 
         public void load(InputStream inputStream) throws Exception {
-            //implement this method - реализуйте этот метод
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            while (reader.ready()) {
+                User user = new User();
+                user.setFirstName(reader.readLine());
+                user.setLastName(reader.readLine());
+                user.setBirthDate(new Date(Long.parseLong(reader.readLine())));
+                user.setMale(Boolean.parseBoolean(reader.readLine()));
+                String s = reader.readLine();
+                switch (s) {
+                    case "Ukraine":
+                        user.setCountry(User.Country.UKRAINE);
+                        break;
+                    case "Russia":
+                        user.setCountry(User.Country.RUSSIA);
+                        break;
+                    default:
+                        user.setCountry(User.Country.OTHER);
+                        break;
+                }
+                this.users.add(user);
+            }
+
+            reader.close();
         }
 
         @Override
